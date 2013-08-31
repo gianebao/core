ca0f8137ed72df054bae2bb776a6155b0d40dd24
Gian Carlo Val Ebao <gianebao@gmail.com>
2013-08-31 21:40:26 +0800

    do a class check first

--- classes/Kohana/UTF8.php
+++ classes/Kohana/UTF8.php
@@ -758,7 +758,7 @@ class Kohana_UTF8 {
 
 }
 
-if (UTF8::$server_utf8 === NULL)
+if (class_exists('UTF8') && UTF8::$server_utf8 === NULL)
 {
 	// Determine if this server supports UTF-8 natively
 	UTF8::$server_utf8 = extension_loaded('mbstring');
